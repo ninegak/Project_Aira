5988d934655ce47732854c7d7ff1ce708a52d1f5
ninegak <zxce2546@gmail.com>
2025-12-23 12:43:01 +0700

    changed code structure

--- aira_brain/src/config.rs
+++
@@ -1,6 +0,0 @@
-pub struct AiraConfig {
-    pub llm_path: &'static str,
-    pub stt_path: &'static str,
-    pub tts_path: &'static str,
-    pub system_prompt: &'static str,
-} 
--- aira_brain/src/lib.rs
+++
@@ -1,5 +0,0 @@
-pub mod aira;
-pub mod stt;
-pub mod llm;
-pub mod tts;
-pub mod config;
